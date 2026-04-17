d920cf8b0a8cd4aee94fcc4244bef432ab3f2ce8
prog-sistem-cun <prog_sistemas@cun.edu.co>
2026-04-16 19:26:30 -0500

    Create preguntas.js

---
+++ preguntas.js
@@ -0,0 +1,17 @@
+const preguntasPrimerSemestre = [
+  { 
+    pregunta: "1. (Intro): ¿Qué software se infiltra sin permiso en un sistema?", 
+    opciones: ["Sistema Operativo", "Malware", "Business Intelligence", "DSS"], 
+    correcta: 1 
+  },
+  { 
+    pregunta: "2. (Programación): Ciclo que se repite mientras se cumple una condición:", 
+    opciones: ["Ciclo For (Para)", "Switch-Case", "Ciclo While (Mientras)", "Condicional If"], 
+    correcta: 2 
+  },
+  { 
+    pregunta: "3. (Circuitos): Ley que relaciona Voltaje, Corriente y Resistencia:", 
+    opciones: ["Ley de Moore", "Ley de Kirchhoff", "Ley de Faraday", "Ley de Ohm"], 
+    correcta: 3 
+  }
+];
